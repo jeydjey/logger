09dea45a1e4f9e9d68722a5b1b94265320ef5e75
Joachim Janiak <joachim.janiak@gmail.com>
2021-02-21 10:24:31 +0100

    added license, added documentation, fixed a small typo in the recursive call to the .sanitize method

--- R/logger.R
+++ R/logger.R
@@ -1,10 +1,37 @@
-Logger <- R6::R6Class("Logger", 
+#' Class to provide logging capabilities
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @keywords logger
+#' @return Object of \code{\link{R6Class}} with methods for logging events to a log file.
+#' @format \code{\link{R6Class}} object.
+#' @examples
+#' my_log <- logger$new("log1.log")
+#' my_log$info("This is an information")
+#' @field file stores the file path
+#' @field custom_sanitizer stores the customer sanitizer list which cleanes logging strings for ndjson format.
+#' @field default_sanitizer stores the default sanitizer list which cleanes logging strings for ndjson format.
+#' #' @section Methods:
+#' \describe{
+#'   \item{\code{new(file)}}{This method is used to create object of this class with \code{file} as path of the logging object it writes to.}
+#'
+#'   \item{\code{add(log_lvl, log_msg, ..., echo = T)}}{This method adds a logging entry to the logfile with wichever \code{log_lvl} defined. \code{...} can be filled with custom named fields to log}
+#'   \item{\code{warn(log_msg, ..., echo = T)}}{This method adds a logging entry to the logfile with "warn" as defined \code{log_lvl}.}
+#'   \item{\code{info(log_msg, ..., echo = T)}}{This method adds a logging entry to the logfile with "info" as defined \code{log_lvl}.}
+#'   \item{\code{err(log_msg, ..., echo = T)}}{This method adds a logging entry to the logfile with "err" as defined \code{log_lvl}.}
+#'   \item{\code{return_log()}}{This method returns the log in the form of a tibble.}
+#'   \item{\code{file(value)}}{This method sets a new path for the logging file.}
+#'   \item{\code{custom_sanitizer(value, add = T)}}{This method adds or replaces custom sanitizer fields.}
+#'   
+logger <- R6::R6Class("logger", 
   public = list(
     initialize = function(file) {
       stopifnot(is.character(file), length(file)==1)
       private$.file <- file
     },
     add = function(log_lvl, log_msg, ..., echo = T) {
+      #add a logging entry with sanitized character strings for ndjson-format
       dots <- list(...)
       if (any(unlist(lapply(dots, length)) > 1)) 
         base::warning("Each custom log field should be of length one, or else the log entry will be shortened to max length of 400 characters")
@@ -15,15 +42,19 @@ Logger <- R6::R6Class("Logger",
       private$.stream_out(log_df, echo = echo)
     },
     warn = function(log_msg, ..., echo = T) {
+      #add a logging entry of type 'warn'
       self$add("warn", log_msg, ..., echo = echo)
     },
     info = function(log_msg, ..., echo = T) {
+      #add a logging entry of type 'info'
       self$add("info", log_msg, ..., echo = echo)
     },
     err = function(log_msg, ..., echo = T) {
+      #add a logging entry of type 'err'
       self$add("err", log_msg, ..., echo = echo)
     },
     return_log =  function() {
+      #returns the written log desanitized and as a tibble
       dplyr::mutate(private$.stream_in(),
                     dplyr::across(where(is.character), .fns = ~private$.sanitize_(.x, T)))
     }
@@ -40,10 +71,10 @@ Logger <- R6::R6Class("Logger",
                       `\n` = "__LF__"),
     .cst_sanitizer = NULL,
     .sanitize = function(text) {
-      
+      #handling of string sanitation for entries with length >1
       if(is.list(text)) {
         if(length(text)>1) {
-          text <- lapply(sanitize)
+          text <- lapply(private$.sanitize)
           return(text)
         } else {
           string <- text[[1]]
@@ -69,6 +100,7 @@ Logger <- R6::R6Class("Logger",
       return(text)
     },
     .sanitize_ = function(string, desanitize = F) {
+      #actual sanitizer/desanitizer method for strings
       sanitizer <- if(is.null(private$.cst_sanitizer)) private$.sanitizer else private$.cst_sanitizer
       for(k in names(sanitizer)) {
         if (!desanitize) {
@@ -81,11 +113,13 @@ Logger <- R6::R6Class("Logger",
       string
     },
     .stream_out = function(df, echo = T) {
+      #write to file
       val <- jsonlite:::asJSON(df, collapse = F)
       write(val, file = private$.file, append = T)
       if(echo) cat(val)
     },
     .stream_in = function() {
+      #read in file
       val_in <- readLines(private$.file)
       val_in <- lapply(val_in, jsonlite::fromJSON)
       dplyr::bind_rows(val_in)
@@ -93,6 +127,7 @@ Logger <- R6::R6Class("Logger",
   ),
   active = list(
     file = function(value) {
+      #return path or set new path
       if(missing(value)) {
         private$.file
       } else {
@@ -103,6 +138,7 @@ Logger <- R6::R6Class("Logger",
       }
     },
     custom_sanitizer = function(value, add = T) {
+      #add or replace custom sanitizer entries to the default sanitizer fields
       if(missing(value)) {
         private$.cst_sanitizer
       } else {
@@ -114,6 +150,7 @@ Logger <- R6::R6Class("Logger",
       }
     },
     default_sanitizer = function(value) {
+      #return the default sanitizer
       if(missing(value)) {
         private$.sanitizer
       } else {
